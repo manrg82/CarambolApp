51c087cc6f164c019b818e950ed75f847dec1acc
manrg82 <manuelruizgutierrez810@gmail.com>
2026-01-06 19:28:10 +0100

    empezada pantalla inicio

--- app/src/main/java/com/carambolapp/MainActivity.kt
+++ app/src/main/java/com/carambolapp/MainActivity.kt
@@ -4,14 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Label
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.helper.widget.Carousel
 import com.carambolapp.ui.theme.CarambolappTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +39,78 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CarambolappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    containerColor = MaterialTheme.colorScheme.background
+                ) { padding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding)
+                            .padding(horizontal = 24.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Header()
+                        Spacer(modifier = Modifier.height(48.dp))
+                        DescriptionText()
+                        Spacer(modifier = Modifier.height(48.dp))
+                        StartButton()
+
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun Header() {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logoappfin),
+                contentDescription = "Carambol Logo",
+                modifier = Modifier.size(80.dp)
+            )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CarambolappTheme {
-        Greeting("Ismallah")
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Text(
+                text = "CarambolApp",
+                style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
     }
+
+    @Composable
+    fun DescriptionText() {
+        Text(
+            text = "Descubre innumerables recetas, gestiona tu despensa de forma inteligente y apoya al comercio local desde nuestra aplicación.",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center
+
+        )
+    }
+
+    @Composable
+    fun StartButton() {
+        Button(
+            onClick = { /* MANDAR A LOGIN */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Text(
+                text = "Empezar",
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+
 }
+
+
